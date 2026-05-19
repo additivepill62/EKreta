088ffce9873d94c5e716e58cdcbd05ab7ce9d9f7
additivepill62 <gabordaniel175@gmail.com>
2026-05-19 13:24:28 +0200

    Redesign user management UI and implement CRUD logic
    Reformatted UserControlFelhasznalok.xaml for clarity and added a new user form with fields for username, full name, password, and role selection. Added Save, Delete, and Modify buttons, with only Save visible by default. Rewrote code-behind to manage user list, populate roles from the Szerepkor enum, and implement database CRUD operations for users. Cleaned up unused usings and comments. SelectionChanged handler is present but not yet implemented.

--- eKreta/UserControls/UserControlFelhasznalok.xaml.cs
+++ eKreta/UserControls/UserControlFelhasznalok.xaml.cs
@@ -1,32 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using eKreta.Models;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace eKreta.UserControls
 {
-    /// <summary>
-    /// Interaction logic for UserControlFelhasznalok.xaml
-    /// </summary>
-    public partial class UserControlFelhasznalok : UserControl
-    {
-        public UserControlFelhasznalok()
-        {
-            InitializeComponent();
-        }
+	/// <summary>
+	/// Interaction logic for UserControlFelhasznalok.xaml
+	/// </summary>
+	public partial class UserControlFelhasznalok : UserControl
+	{
+		List<Felhasznalo> felhasznalok;
+		Felhasznalo kivalasztottFelhasznalo;
+
+		public UserControlFelhasznalok()
+		{
+			InitializeComponent();
+			szerepkorCBOX.ItemsSource = Enum.GetValues(typeof(Szerepkor));
+			AdatbazisLekerdezes();
+			felhasznalok = new List<Felhasznalo>();
+		}
+
+		private void AdatbazisLekerdezes()
+		{
+			//elemek 0-ra
+
+			var felhasznaloRepo = new GenericRepository<Felhasznalo>(App.databasePath);
+			var lekerdezes = felhasznaloRepo.GetAll();
+			datagridFelhasznalok.ItemsSource = lekerdezes;
+
+
+		}
 
 		private void datagridFelhasznalok_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
+
+		}
+
+		private void mentesBTN_Click(object sender, RoutedEventArgs e)
+		{
+			//Szerepkör
+			string kivalasztottSzerepkorNev = szerepkorCBOX.SelectedItem.ToString();
+			Szerepkor kivalasztottSzerepkor = (Szerepkor)Enum.Parse(typeof(Szerepkor), kivalasztottSzerepkorNev);
+			int kivalasztottSzerepkorId = (int)kivalasztottSzerepkor;
+
+			//Új felhasználó létrehozása a megadott adatokkal
+			Felhasznalo ujFelhasznalo = new Felhasznalo(felhasznalonevTBOX.Text, teljesnevTBOX.Text, jelszoTBOX.Password, kivalasztottSzerepkorId);
+
+			//adatbázisba mentés
+			var felhasznaloRepo = new GenericRepository<Felhasznalo>(App.databasePath);
+			felhasznaloRepo.Insert(ujFelhasznalo);
+
+			//datagrid frissítése
+			AdatbazisLekerdezes();
+
+		}
+
+		private void torlesBTN_Click(object sender, RoutedEventArgs e)
+		{
+			//adatbázisba mentés
+			var felhasznaloRepo = new GenericRepository<Felhasznalo>(App.databasePath);
+
+			felhasznaloRepo.Delete(kivalasztottFelhasznalo);
+
+			//datagrid frissítése
+			AdatbazisLekerdezes();
+		}
+
+		private void modBTN_Click(object sender, RoutedEventArgs e)
+		{
+			kivalasztottFelhasznalo.FelhasznaloNev = felhasznalonevTBOX.Text;
+			kivalasztottFelhasznalo.TeljesNev = teljesnevTBOX.Text;
+
+			string kivalasztottSzerepkorNev = szerepkorCBOX.SelectedItem.ToString();
+			Szerepkor kivalasztottSzerepkor = (Szerepkor)Enum.Parse(typeof(Szerepkor), kivalasztottSzerepkorNev);
+			kivalasztottFelhasznalo.Szerepkor = (int)kivalasztottSzerepkor;
+
+			if(jelszoTBOX.Password != "")
+			{
+				kivalasztottFelhasznalo.Jelszo = jelszoTBOX.Password; // TODO: hash jelszó
+			}
+
+
+
+
+
+
 
 		}
 	}
